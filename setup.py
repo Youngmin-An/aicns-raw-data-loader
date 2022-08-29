d0994146ef3120185ce7059f777eab403a6c2e97
Youngmin-An <youngmin.develop@gmail.com>
2022-08-29 16:41:30 +0900

    [AICNS-23] Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name="aicns_raw_data_loader",
+      version="0.0.1",
+      description="Raw data loader library package in AICNS project",
+      author="Youngmin An",
+      author_email="youngmin.develop@gmail.com",
+      license="Apache License 2.0",
+      url="https://github.com/Youngmin-An/aicns-raw-data-loader",
+      packages=find_packages(),
+      install_requires=[
+          'pendulum',
+          'pyspark>=3.0.3',
+          'aicns-feature-metadata'
+      ]
+)
+
